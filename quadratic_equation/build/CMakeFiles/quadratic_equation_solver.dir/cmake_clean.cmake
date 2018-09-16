2659bbeb5e74c5c76e0ce6bd54739af312a29df3
mb <mihbogomolov@gmail.com>
2018-09-16 13:51:51 +0300

    moved to subdirectory

---
+++ quadratic_equation/build/CMakeFiles/quadratic_equation_solver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quadratic_equation_solver.dir/Tests.cpp.o"
+  "quadratic_equation_solver.pdb"
+  "quadratic_equation_solver"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quadratic_equation_solver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ quadratic_equation/docs/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['belowzero',['belowZero',['../quadratic__equation__solver_8h.html#a8f3e514cd3a83b553bd6fc53557ae172',1,'quadratic_equation_solver.h']]]
+];
